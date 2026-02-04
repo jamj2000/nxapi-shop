b8f1a2e8f3271b867e3daadac8fe796a77542d3b
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2026-02-04 14:51:23 +0100

    patch method for product id

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -87,7 +87,10 @@ export async function PATCH(request, { params }) {
     }
 
     try {
-        const product = await prisma.product.findUnique({ where: { id } });
+        const product = await prisma.product.findUnique({
+            where: { id },
+            include: { images: true }
+        });
 
         if (!product) {
             return NextResponse.json(
@@ -97,13 +100,26 @@ export async function PATCH(request, { params }) {
         }
 
         const body = await request.json();
+        const { title, price, description, slug, stock, sizes, gender, tags, images = [] } = body;
 
         const updatedProduct = await prisma.product.update({
             where: { id },
             data: {
-                ...body,
-                ...(body.images &&
-                    { images: { create: body.images.map(image => ({ url: image })) } }),
+                title,
+                price,
+                description,
+                slug,
+                stock,
+                sizes,
+                gender,
+                tags,
+                images: {
+                    connectOrCreate: images.map(url => ({
+                        where: { url },
+                        create: { url }
+                    }))
+                },
+                userId: idUser,
             },
             // incluimos las imágenes en la respuesta
             include: {
